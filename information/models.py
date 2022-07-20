c10315b49053fd4dc70d40bbf64371153dd2ea08
Yoshio Mori <yoshio.mori@hotmail.com>
2022-07-20 12:29:37 -0300

    Display object as name

--- information/models.py
+++ information/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Departament(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Employ(models.Model):
     name = models.CharField(max_length=747)  # largest name 747 Ref.: https://www.guinnessworldrecords.com/world-records/67285-longest-personal-name#:~:text=The%20longest%20personal%20name%20is,verified%20on%201%20January%202021.
@@ -12,3 +15,6 @@ class Employ(models.Model):
 
     class Meta:
         verbose_name_plural = "Employees"
+
+    def __str__(self):
+        return self.name
